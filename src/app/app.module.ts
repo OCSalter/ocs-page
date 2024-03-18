c15019079252f5b332db00994dd82252a1d4c7da
Owen Salter <owensalter@outlook.com>
2024-03-18 17:05:53 -0400

    Add intial changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ResumeEntriesComponent } from './resume-entries/resume-entries.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ResumeEntriesComponent,
   ],
   providers: [
     provideClientHydration()
