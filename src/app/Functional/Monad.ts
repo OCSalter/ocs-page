87e9a498e8ef1391ebf268318aa81a2c38430883
Owen Salter <owensalter@outlook.com>
2024-03-28 09:29:58 -0400

    Add either and option

--- src/app/Functional/Monad.ts
+++ src/app/Functional/Monad.ts
@@ -2,13 +2,17 @@ export interface Monad<T>{
     flatMap<A>(f: (_: T) => A) : Monad<A>;
 }
 
-export class Option<T> implements Monad<T>{
+export class Option<T>{
 
     constructor(private item : T | undefined) {}
 
-    public flatMap<A>( f: (_: T) => A) : Option<A> {
-        return new Option<A>(this.item ? f(this.item) : undefined);
+    public map<A>(f: (_: T) => A): Option<A> {
+        return this.item ? new Option(f(this.item)) : None<A>();
     }
+
+    public flatMap<A>(f: (_: T) => Option<A>) : Option<A> {
+        return this.item ? f(this.item) : None<A> ();
+    } 
 
     get(): T | undefined {
         return this.item;
@@ -21,4 +25,37 @@ export function Some<T>(_: T): Option<T> {
 
 export function None<T>(): Option<T> {
     return new Option<T>(undefined);
+}
+
+
+export class Either<T> {
+    constructor(private right?: T, private left?: any) { }
+
+    public map<A> (f: (_: T) => A) : Either<A> {
+        return this.right ? 
+            Right (f(this.right)) : 
+            Left (this.left);
+    }
+
+    public flatMap<A>(f: (_: T) => Either<A>) : Either<A> {
+        return this.right ? 
+            (f(this.right)) : 
+            Left (this.left);
+    }
+
+    public getRight(): T | undefined {
+        return this.right;
+    }
+
+    public getLeft(): any {
+        return this.left;
+    }
+}
+
+export function Right<T>(_: T): Either<T> {
+    return new Either<T>(_, undefined);
+}
+
+export function Left<T>(_: any): Either<T> {
+    return new Either<T>(undefined, _ ? _ : new Error("Generated Either with no Arguments"));
 }
